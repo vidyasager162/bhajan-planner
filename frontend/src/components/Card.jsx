b34f8b3c949b59de9d863c9b5fa52a05a472507e
Vidyasager <vidyasager162@gmail.com>
2023-08-15 17:39:03 +0530

    Finish till Div Clickable

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card(props) {
   return (
-    <div className="card h-100">
+    <div className="card h-100" onClick={props.clicked}>
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
       </div>
--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import Card from "./Card";
 
 function Main() {
+  function clicked() {
+    alert("Clicked!!");
+  }
   return (
     <div className="container-fluid">
       <div className="row main-container">
         <div className="col main-options">
-          <Card title="Bhajans" />
+          <Card title="Bhajans" clicked={clicked} />
         </div>
         <div className="col main-options">
-          <Card title="Deities" />
+          <Card title="Deities" clicked={clicked} />
         </div>
         <div className="col main-options">
-          <Card title="I Dont know" />
+          <Card title="I Dont know" clicked={clicked} />
         </div>
       </div>
     </div>
