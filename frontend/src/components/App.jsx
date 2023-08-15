4b61d708268c668068d72d98e9a7a395ad8b0d51
Vidyasager <vidyasager162@gmail.com>
2023-08-15 17:42:36 +0530

    Commented some

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,8 +3,8 @@ import Header from "./Header";
 import Main from "./Main";
 import Home from "./Home";
 import Footer from "./Footer";
-import uniqid from "uniqid";
-import { useCookies } from "react-cookie";
+// import uniqid from "uniqid";
+// import { useCookies } from "react-cookie";
 import Login from "./Login";
 
 function App() {
@@ -14,9 +14,9 @@ function App() {
   }, []);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [toLogin, setToLogin] = useState(false);
-  const [cookie, setCookie] = useCookies(["userSaved", "username", "password"]);
-  const [checkForCookies, setCheckForCookies] = useState(true);
-  const [User, setUser] = useState();
+  // const [cookie, setCookie] = useCookies(["userSaved", "username", "password"]);
+  // const [checkForCookies, setCheckForCookies] = useState(true);
+  // const [User, setUser] = useState();
 
   function checkAdmin() {
     fetch("http://127.0.0.1:8000/check-admin", {
